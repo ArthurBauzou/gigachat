6397284442e7a10901ca4aeda72b764743d51e80
root <root@guest.guest>
2022-06-12 18:29:46 +0000

    ca va etre douloureux

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const io = require('socket.io');
+
+const { Server } = require('socket.io');
+// const { createSerer } = require('hhtp');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { createServer } = require('http');
 
-var app = express();
+// SOCKET INIT
+const app = express();
+const httpServer = createServer(app)
+const io = new Server(httpServer,{})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// io.on('connection', (socket) => {
-//   console.log('user connected')
-// })
+// SOCKET
+io.on('connection', (socket) => {
+  console.log('user connected')
+})
+httpServer.listen(3000)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
